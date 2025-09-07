a18bd8f354fea72c6775d5781e48bd0c33d9982e
gyhmac <gyeonho@gmail.com>
2025-09-08 08:53:47 +0900

    Daily Coding -P10814

---
+++ src/BaekJoonCote/P10814.java
@@ -0,0 +1,38 @@
+package BaekJoonCote;
+
+import java.util.*;
+/*
+    * 백준 10814번 나이순 정렬
+    * https://www.acmicpc.net/problem/10814
+    * Silver 5
+    * 2024-06-12
+
+    * 문제
+    * 온라인 저지에 가입한 사람들의 나이와 이름이 가입한 순서대로 주어진다.
+    * 이때, 회원들을 나이가 증가하는 순으로,
+    * 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서로 정렬하는 프로그램을 작성하시오.
+ */
+public class P10814 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        int n = sc.nextInt();
+        sc.nextLine(); // 개행 문자 제거
+        List<Member> members = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int age = sc.nextInt();
+            String name = sc.next();
+            members.add(new Member(age, name));
+        }
+    }
+
+    class Member {
+        int age;
+        String name;
+
+        Member(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
+}
